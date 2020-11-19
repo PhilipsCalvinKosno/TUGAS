0550e1f6c696bae3cd99cb74bac4eb6b8a606663
PhilipsCalvinKosno <72179377+PhilipsCalvinKosno@users.noreply.github.com>
2020-11-19 12:12:45 +0800

    Add files via upload

---
+++ mid praktek/Enkapsulasi/Barang.java
@@ -0,0 +1,11 @@
+
+public class Barang extends main{
+	private int total;	
+	
+	void datajumlah(int jumlah, int bayar, int kembalian, int harga){
+	
+	
+       System.out.println("Total Harga = Rp "+total);
+      
+		}
+		}
---
+++ mid praktek/Enkapsulasi/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main{
+	void sub (String nama, int total,int bayar,int kembalian){
+	
+		   
+	   
+		
+	   System.out.println("Nama        : "+nama );
+	   System.out.println("Total Harga : Rp."+total);
+	   System.out.println("Bayar       : Rp."+bayar);
+	   System.out.println("-------------------------------------------");
+	   System.out.println("Kembalian   : Rp."+kembalian);
+	   }
+}
